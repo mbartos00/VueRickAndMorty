86268f0323e9db84cf7584cce81d966d97d3d0cb
Maciej Bartos <mbartos@griddynamics.com>
2024-08-26 13:51:19 +0200

    feat: add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-pink rounded-lg shadow-lg p-6 max-w-xs text-center">
+    <img :src="character.image" alt="Character Image" class="w-full h-auto rounded-lg mb-4" />
+    <div class="space-y-2">
+      <h2 class="text-xl font-semibold">{{ character.name }}</h2>
+      <div class="flex gap-2 justify-center">
+        <p class="text-accent"><strong>Species:</strong> {{ character.species }}</p>
+        <p class="text-accent"><strong>Status:</strong> {{ character.status }}</p>
+      </div>
+    </div>
+    <button
+      class="mt-4 px-4 py-2 bg-accent text-white rounded transition-all duration-150 hover:bg-green hover:text-accent"
+      @click="toggleFavourite()"
+    >
+      {{ isFav ? 'Remove from favourites' : 'Add to favourites' }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { defineProps } from 'vue';
+import { Character } from '../types/types';
+import { useFavouritesStore } from '../store/store';
+
+const { isFavourite, addFavourite, removeFavourite } = useFavouritesStore();
+
+const props = defineProps<{ character: Character }>();
+
+const isFav = computed(() => isFavourite(props.character.id));
+
+const toggleFavourite = () =>
+  isFav.value ? removeFavourite(props.character.id) : addFavourite(props.character);
+</script>
