04aceded355719fa6ec113fafe5a2b3e3c3f71f8
Maciej Bartos <mbartos@griddynamics.com>
2024-08-26 13:38:47 +0200

    feat: add transition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <div class="bg-primary w-full min-h-[100svh]">
+    <Header />
+    <main>
+      <RouterView v-slot="{ Component }">
+        <Transition
+          enter-active-class="duration-300 ease-out"
+          enter-from-class="transform opacity-0 scale-75"
+          enter-to-class="opacity-100 scale-100"
+          leave-active-class="duration-200 ease-in"
+          leave-from-class="opacity-100 scale-100"
+          leave-to-class="transform opacity-0 scale-75"
+        >
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
+    </main>
+  </div>
 </template>
